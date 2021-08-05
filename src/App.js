e22ad2800622455d3fec23884456aed716621fb1
cdp <cdp@cdp>
2021-08-05 09:44:42 -0500

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Dios es fuente de vida para todos
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=NbewIIQsFCo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
